2a42654d89caf186a5afe3dc77a842547bc9262e
LyHaTik <LyHaTik12@yandex.ru>
2025-05-24 16:31:24 +0300

    Готова v0.1

--- car_showroom/car_showroom/settings.py
+++ car_showroom/car_showroom/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ql5qaz^ffbywf#g!_te1eq4ugl_01y%lqtgz)tdj-f5+_gm*ne'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
